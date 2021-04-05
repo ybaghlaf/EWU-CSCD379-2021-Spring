89d99028f16cd8983b6ae7d2e5258c3fe5685ec7
Yousef Baghlaf <ybaghlaf@eagles.ewu.edu>
2021-04-05 14:42:25 +0300

    Edited both files and added screenshots

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -32,7 +32,7 @@ namespace SecretSanta.Api
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello from API!");
+                    await context.Response.WriteAsync("Hello, this is Yousef's edit of API file.");
                 });
             });
         }
--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -32,7 +32,7 @@ namespace SecretSanta.Web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello from Web!");
+                    await context.Response.WriteAsync("Hello, this is Yousef's edit of the Web file.");
                 });
             });
         }
